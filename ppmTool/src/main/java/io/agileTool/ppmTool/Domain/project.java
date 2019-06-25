2d456460de2223826b6a691865e3611d8c7fe1c3
Dipika <dipika.rauniyar@gmail.com>
2019-06-25 00:12:55 -0400

    setting up h2 database

--- ppmTool/src/main/java/io/agileTool/ppmTool/Domain/project.java
+++ ppmTool/src/main/java/io/agileTool/ppmTool/Domain/project.java
@@ -1,4 +1,99 @@
 package io.agileTool.ppmTool.Domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String projectName;
+    private  String projectIdentifier;
+    private String description;
+    private Date startDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    private Date endDate;
+    private Date created_At;
+    private Date updated_At;
+
+    public project() {
+    }
+
+    @PrePersist
+    protected  void createdAt() {
+        this.created_At = new Date();
+
+    }
+
+    @PreUpdate
+    protected  void updatedAt() {
+        this.updated_At = new Date();
+
+    }
 }
--- ppmTool/src/main/java/io/agileTool/ppmTool/PpmToolApplication.java
+++ ppmTool/src/main/java/io/agileTool/ppmTool/PpmToolApplication.java
@@ -2,8 +2,10 @@ package io.agileTool.ppmTool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"Domain"})
 public class PpmToolApplication {
 
 	public static void main(String[] args) {
