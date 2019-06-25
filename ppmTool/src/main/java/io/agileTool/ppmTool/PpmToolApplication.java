f55a4960246606ce34ebda94a36f3ba439f963c0
Dipika <dipika.rauniyar@gmail.com>
2019-06-25 00:27:43 -0400

    project table created

--- ppmTool/src/main/java/io/agileTool/ppmTool/PpmToolApplication.java
+++ ppmTool/src/main/java/io/agileTool/ppmTool/PpmToolApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"Domain"})
 public class PpmToolApplication {
 
 	public static void main(String[] args) {
